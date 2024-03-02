b4a38fba688136dd4f94df9a97dea54e1a87708a
Immortal <ircfspace@gmail.com>
2024-03-02 22:58:33 +0330

    fixed: toast

--- src/helpers/copyIPToClipboard.ts
+++ src/helpers/copyIPToClipboard.ts
@@ -2,7 +2,14 @@ import { toast } from "react-hot-toast";
 export async function copyIPToClipboard(ip: string) {
   try {
     await navigator.clipboard.writeText(ip);
-    toast.success(`IP ${ip} copied to clipboard!`);
+    toast.success(`${ip} copied!`, {
+      position:"bottom-center",
+      style: {
+        borderRadius: '10px',
+        background: '#333',
+        color: '#fff',
+      },
+    });
   } catch (error) {
     toast.error(`Failed to copy IP ${ip} to clipboard!`);
     console.error(error);
