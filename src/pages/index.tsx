51c34bea89a154e6466748f5258d9ad2c6b997d2
Immortal <ircfspace@gmail.com>
2024-03-02 12:52:12 +0330

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@
 import { type NextPage } from "next";
 import dynamic from "next/dynamic";
 import Head from "next/head";
-import Image from "next/image";
 import { type TryChar, useIPScanner } from "~/hooks/useIPScanner";
 import { download } from "~/helpers/download";
 import {
