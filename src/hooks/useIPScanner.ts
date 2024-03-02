176871b738b009ab01d1413b5b9e6ec9d94ee3dd
Immortal <ircfspace@gmail.com>
2024-03-02 12:47:05 +0330

    Update useIPScanner.ts

--- src/hooks/useIPScanner.ts
+++ src/hooks/useIPScanner.ts
@@ -161,7 +161,7 @@ export const useIPScanner = ({ allIps }: IPScannerProps) => {
   async function testIPs(ipList: string[]) {
     for (const ip of ipList) {
       increaseTestNo();
-      const url = `https://${ip}/__down`;
+      const url = `http://${ip}/__down`;
 
       let testCount = 0;
 
