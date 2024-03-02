ad0ed636fa9eff7f6a3eca60017351a4ec8eff0b
kiomarzsss <kiomarzsss@gmail.com>
2024-03-02 05:02:21 -0500

    feat: PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,23 @@
 /** @type {import("next").NextConfig} */
+
+import withSerwistInit from "@serwist/next";
+
 const isProd = process.env.NODE_ENV === 'production';
 
-const config = {
+const withSerwist = withSerwistInit({
+  disable: !isProd,
+  swSrc: "src/sw.ts",
+  swDest: "public/sw.js",
+});
+
+const nextConfig = {
   output: 'export',
   images: {   
     unoptimized: true,
   },  
   reactStrictMode: true,
 };
-export default config;
+
+export default withSerwist({
+  ...nextConfig
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,12 +48,77 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Cloudflare Scanner</title>
-        <meta
-          name="description"
-          content="Cloudflare Scanner to find clean ip"
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>kscanner</title>
+        <meta name="description" content="Cloudflare Clean IP Scanner" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="mask-icon" href="/icons/mask-icon.svg" color="#FFFFFF" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/touch-icon-ipad.png"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/touch-icon-iphone-retina.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="167x167"
+          href="/icons/touch-icon-ipad-retina.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://scanner.github1.cloud/" />
+        <meta name="twitter:title" content="kscanner" />
+        <meta name="twitter:description" content="Cloudflare Clean IP Scanner" />
+        <meta name="twitter:image" content="/icons/twitter.png" />
+        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="kscanner" />
+        <meta property="og:description" content="Cloudflare Clean IP Scanner" />
+        <meta property="og:site_name" content="kscanner" />
+        <meta property="og:url" content="https://scanner.github1.cloud/" />
+        <meta property="og:image" content="/icons/og.png" />
+        {/* add the following only if you want to add a startup image for Apple devices. */}
+        {/* <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_2048.png"
+          sizes="2048x2732"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_1668.png"
+          sizes="1668x2224"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_1536.png"
+          sizes="1536x2048"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_1125.png"
+          sizes="1125x2436"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_1242.png"
+          sizes="1242x2208"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_750.png"
+          sizes="750x1334"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_640.png"
+          sizes="640x1136"
+        /> */}
       </Head>
       <main className="flex h-full max-w-full flex-col items-center justify-center bg-gradient-to-b from-cyan-300 to-cyan-400 px-3">
         <div className="max-h-full w-[900px] max-w-full rounded-lg bg-slate-200 p-5 text-gray-700 shadow-lg">
