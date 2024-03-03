c978a7d875abc8b1a3a9635ff8705090f125877d
Immortal <ircfspace@gmail.com>
2024-03-03 20:26:43 +0330

    Update randomizeElements.ts

--- src/helpers/randomizeElements.ts
+++ src/helpers/randomizeElements.ts
@@ -2,7 +2,7 @@ export function randomizeElements<T>(arr: T[]): T[] {
     const result: T[] = [...arr];
     for (let i: number = result.length - 1; i > 0; i--) {
         const j: number = Math.floor(Math.random() * (i + 1));
-        [result[i], result[j]] = [result[j], result[i]];
+        [result[i]!, result[j]!] = [result[j]!, result[i]!];
     }
     return result.slice(0, 100);
 }
