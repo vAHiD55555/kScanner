4873877925c7d7be8188d2cb8a61d985d55b2dcf
Immortal <ircfspace@gmail.com>
2024-03-06 11:25:39 +0330

    randomizeElements

--- src/helpers/randomizeElements.ts
+++ src/helpers/randomizeElements.ts
@@ -1,6 +1,6 @@
 export function randomizeElements<T>(arr: T[]) {
     let result = [...arr].sort(() => 0.5 - Math.random() );
-    result = result.slice(0, 100);
+    result = result.slice(0, 150);
     result = result.sort(() => 0.5 - Math.random() );
     result = result.sort(() => 0.5 - Math.random() );
     return result;
